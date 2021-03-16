5a8a1bca1b244674fde8cc7524504c5393218c92
zf1976 <1596863112@qq.com>
2021-03-16 20:54:51 +0800

    fix: 修复依赖版本号管理 feat: gateway新增feign

--- ant-gateway/src/main/java/com/zf1976/ant/gateway/GatewayApplication.java
+++ ant-gateway/src/main/java/com/zf1976/ant/gateway/GatewayApplication.java
@@ -1,19 +1,33 @@
 package com.zf1976.ant.gateway;
 
+import com.zf1976.ant.gateway.feign.ClientTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 /**
  * @author mac
  */
 @SpringBootApplication(scanBasePackages = "com.zf1976")
 @EnableDiscoveryClient
+@EnableFeignClients
+@RestController
 public class GatewayApplication {
+
+    @Autowired
+    private ClientTest clientTest;
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-
+    @GetMapping("/oauth/demo")
+    public Mono<Object> demo(){
+        return Mono.just(clientTest.test());
+    }
 }
