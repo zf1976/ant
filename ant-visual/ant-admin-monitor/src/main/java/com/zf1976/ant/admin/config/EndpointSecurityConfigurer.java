11cb84ccc6d153a945550fb7fb257bdfd8609623
zf1976 <1596863112@qq.com>
2021-05-26 00:52:42 +0800

    feat: 完善Admin监控模块安全

--- ant-visual/ant-admin-monitor/src/main/java/com/zf1976/ant/admin/config/EndpointSecurityConfigurer.java
+++ ant-visual/ant-admin-monitor/src/main/java/com/zf1976/ant/admin/config/EndpointSecurityConfigurer.java
@@ -3,6 +3,7 @@ package com.zf1976.ant.admin.config;
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -37,27 +38,30 @@ public class EndpointSecurityConfigurer extends WebSecurityConfigurerAdapter {
         SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
         successHandler.setTargetUrlParameter("redirectTo");
         final String adminServerContextPath = this.adminServer.getContextPath();
-        successHandler.setDefaultTargetUrl(adminServerContextPath +"/");
+        successHandler.setDefaultTargetUrl(adminServerContextPath +"/applications");
 
         http.authorizeRequests()
             .antMatchers(adminServerContextPath +  "/assets/**").permitAll()
-            .antMatchers(adminServerContextPath
-
-                    + "/login").permitAll()
+            .antMatchers(adminServerContextPath + "/login").permitAll()
             .anyRequest().authenticated()
             .and()
-            .formLogin().loginPage(adminServerContextPath  + "/login").successHandler(successHandler).and()
-            .logout().logoutUrl(adminServerContextPath +  "/logout").and()
-            .httpBasic().and()
-            .csrf()
-            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-            .ignoringRequestMatchers(
-                    new AntPathRequestMatcher(adminServerContextPath +  "/instances", HttpMethod.POST.toString()),
-                    new AntPathRequestMatcher(adminServerContextPath +  "/instances/*", HttpMethod.DELETE.toString()),
-                    new AntPathRequestMatcher(adminServerContextPath  + "/actuator/**")
-            )
+            .formLogin().loginPage(adminServerContextPath  + "/login").permitAll()
+            .successHandler(successHandler).permitAll()
             .and()
-            .rememberMe().key(UUID.randomUUID().toString()).tokenValiditySeconds(1209600);
+            .logout().logoutUrl(adminServerContextPath +  "/logout").permitAll()
+            .logoutSuccessUrl(adminServerContextPath + "/logout").permitAll()
+            .and()
+            .httpBasic()
+            .and()
+            // 防止iframe跨域
+            .headers().frameOptions().disable()
+            .and()
+            // 跨域开启
+            .cors().disable()
+            // 关闭csrf
+            .csrf().disable()
+            // 关闭记住模式
+            .rememberMe().disable();
 
     }
 }
